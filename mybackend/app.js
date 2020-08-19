ffd18aa4eff001daec9aafad7727160046a67b21
Juan David Martinez <jdavid.martinez@gmail.com>
2020-08-19 13:58:13 -0500

    App que discrimina pacientes covid en Colombia por edad y genero

---
+++ mybackend/app.js
@@ -0,0 +1,21 @@
+const express = require('express')();
+const app = express;
+const request = require('request');
+const bodyParser = require('body-parser');
+
+const port = 5000;
+const url 	= "https://www.datos.gov.co/resource/gt2j-8ykr.json"
+
+app.get('/', (req, res) => res.send('hello World'));
+
+app.get('/pacientes', (req, res) => {
+    request (url, function(error, response, body) {
+        if(!error &&  response.statusCode == 200){
+            let parseBody = JSON.parse(body);
+            let result = parseBody[100]['edad'];
+            res.send(result);
+        }
+    })
+});
+
+app.listen(port,() => console.log(`App listenin on port ${port}!`));
