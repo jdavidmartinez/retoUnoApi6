74aac8bf82e63eb66a7bab414be076dab16d87b7
Juan David Martinez <jdavid.martinez@gmail.com>
2020-08-19 22:49:55 -0500

    Veesion con todos los criterios de seleccion, falta trabajar en el diseño de la GUI

--- mybackend/app.js
+++ mybackend/app.js
@@ -8,11 +8,47 @@ const url 	= "https://www.datos.gov.co/resource/gt2j-8ykr.json"
 
 app.get('/', (req, res) => res.send('hello World'));
 
-app.get('/pacientes', (req, res) => {
+app.get('/pacientesmasculinos', (req, res) => {
     request (url, function(error, response, body) {
         if(!error &&  response.statusCode == 200){
             let parseBody = JSON.parse(body);
             let result = parseBody.filter(val => val.sexo === 'M')
+            res.send(result);
+        }
+    })
+});
+app.get('/pacientesfemeninos', (req, res) => {
+    request (url, function(error, response, body) {
+        if(!error &&  response.statusCode == 200){
+            let parseBody = JSON.parse(body);
+            let result = parseBody.filter(val => val.sexo === 'F')
+            res.send(result);
+        }
+    })
+});
+app.get('/pacientesveinte', (req, res) => {
+    request (url, function(error, response, body) {
+        if(!error &&  response.statusCode == 200){
+            let parseBody = JSON.parse(body);
+            let result = parseBody.filter(val => val.edad <= 20)
+            res.send(result);
+        }
+    })
+});
+app.get('/pacientescuarenta', (req, res) => {
+    request (url, function(error, response, body) {
+        if(!error &&  response.statusCode == 200){
+            let parseBody = JSON.parse(body);
+            let result = parseBody.filter(val => val.edad >20 && val.edad <= 40)
+            res.send(result);
+        }
+    })
+});
+app.get('/pacientesmayores', (req, res) => {
+    request (url, function(error, response, body) {
+        if(!error &&  response.statusCode == 200){
+            let parseBody = JSON.parse(body);
+            let result = parseBody.filter(val =>  val.edad > 40)
             res.send(result);
         }
     })
